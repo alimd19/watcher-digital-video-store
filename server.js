1165652758187f7c74ed5cc4c322bbc2a51bd3ef
Ali Mohamad <wadhawaniaalimohamad@gmail.com>
2022-06-30 21:07:02 -0400

    change db port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middleware = jsonServer.defaults({
   static: "./build",
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 server.use(middleware);
 server.use(
   jsonServer.rewriter({
