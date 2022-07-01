7e408e343bd3b08c2698ce91f6d48ef993667cc1
Ali Mohamad <wadhawaniaalimohamad@gmail.com>
2022-06-30 22:43:37 -0400

    set port

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./data/db.json");
 const middleware = jsonServer.defaults({ static: "./build" });
-const port = process.env.PORT || 8000;
+const port = 8000;
 
 server.use(middleware);
 server.use(router);
 
 server.listen(port, () =>
-  console.log(`Successfullyyyyy started server on port: ${port}`)
+  console.log(`Successfully started server on port: ${port}`)
 );
